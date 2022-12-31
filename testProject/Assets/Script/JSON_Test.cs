67860af6ba6ea18a11e775ed22e84ff3f9e9e7c1
LimTurtle <pigwoo98@gmail.com>
2023-01-01 00:24:59 +0900

    JSON 사용법 추가 #2

---
+++ testProject/Assets/Script/JSON_Test.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class JSON_Test : MonoBehaviour
+{
+    public Data player;
+    [ContextMenu("To Json Data")]
+    void SavePlayerDataToJson()
+    {
+        string jsonData = JsonUtility.ToJson(player, true);
+        string path = Path.Combine(Application.dataPath, "player_data.json");
+        File.WriteAllText(path, jsonData);
+    }
+
+    [ContextMenu("From Json Data")]
+    void LoadPlayerDataToJson()
+    {
+        string path = Path.Combine(Application.dataPath, "player_data.json");
+        string jsonData = File.ReadAllText(path);
+        player = JsonUtility.FromJson<Data>(jsonData);
+    }
+}
+
+[System.Serializable]
+public class Data
+{
+    public string name;
+    public int level;
+    public int coin;
+    public bool skill;
+}
